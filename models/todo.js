c902bcc72c6d70d906437343762137e5a6df51bd
jonathantang <jonathantang0126@gmail.com>
2019-09-07 17:49:03 +0800

    Add done in model, and create checkbox in index, detail, and edit pages

--- models/todo.js
+++ models/todo.js
@@ -5,6 +5,10 @@ const todoSchema = new Schema({
   name: {
     type: String,               // 資料型別是字串
     required: true          // 這是個必填欄位
+  },
+  done: {                             // 「完成狀態」
+    type: Boolean,
+    default: false               // 預設完成狀態為 false
   }
 })
 
