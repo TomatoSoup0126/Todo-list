f4cf5773a2714cb5ab99836b7f969cd75be61675
jonathantang <jonathantang0126@gmail.com>
2019-09-11 22:07:12 +0800

    Add user authentication in todo and home routers

--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,12 @@
 const express = require('express')
 const router = express.Router()
 const Todo = require('../models/todo')
+
+// 載入 auth middleware 裡的 authenticated 方法
+const { authenticated } = require('../config/auth')
+
 // 設定首頁路由器
-router.get('/', (req, res) => {
+router.get('/', authenticated, (req, res) => {
   Todo.find({})
     .sort({ name: 'asc' })
     .exec((err, todos) => {
